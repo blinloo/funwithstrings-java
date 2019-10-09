bd6457950a41fb86a3e9ad731d72ab6101ad67f5
Ruby <blinlooiijhuuyh@gmail.com>
2019-10-09 16:15:28 +0100

    project created
    basic input java console code

---
+++ PigLatin/src/net/fun/Main.java
@@ -0,0 +1,14 @@
+package net.fun;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+        Scanner s = new Scanner(System.in);
+        System.out.print( "Enter Text: "  );
+        String inputText = s.nextLine();
+        //System.out.println("Your text in Pig Latin: ", outputText);
+    }
+}
